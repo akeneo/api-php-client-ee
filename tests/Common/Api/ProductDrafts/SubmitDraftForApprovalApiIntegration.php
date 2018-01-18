83bf98fa7c53da57eabffba02e17177850540296
Damien Carcel <damien.carcel@akeneo.com>
2018-01-18 10:34:41 +0100

    Tag unstable integration tests

--- tests/Common/Api/ProductDrafts/SubmitDraftForApprovalApiIntegration.php
+++ tests/Common/Api/ProductDrafts/SubmitDraftForApprovalApiIntegration.php
@@ -4,8 +4,10 @@ namespace Akeneo\PimEnterprise\ApiClient\tests\Common\Api\ProductDrafts;
 
 /**
  * @author    Damien Carcel <damien.carcel@gmail.com>
- * @copyright 2017 Akeneo SAS (http=>//www.akeneo.com)
- * @license   http=>//opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * @group unstable
  */
 class SubmitDraftForApprovalApiIntegration extends AbstractProductDraftApiTestCase
 {
