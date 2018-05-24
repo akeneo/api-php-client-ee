d0ba55b686a7f5050173ba4e5c832912b4c71d17
Elodie Raposo <raposo.elodie@gmail.com>
2018-05-24 11:21:38 +0200

    API-599: add end-to-end tests for download and upload AssetReferenceFile and AssetVariationFile

---
+++ tests/Api/AssetReferenceFile/DownloadAssetReferenceFileIntegration.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Akeneo\PimEnterprise\ApiClient\tests\Api\AssetReferenceFile;
+
+use Akeneo\PimEnterprise\ApiClient\Api\AssetReferenceFileApi;
+use Akeneo\PimEnterprise\ApiClient\tests\Api\ApiTestCaseEnterprise;
+use donatj\MockWebServer\RequestInfo;
+use donatj\MockWebServer\Response;
+use donatj\MockWebServer\ResponseStack;
+use PHPUnit\Framework\Assert;
+use Psr\Http\Message\StreamInterface;
+
+class DownloadAssetReferenceFileIntegration extends ApiTestCaseEnterprise
+{
+    public function test_download_a_localizable_asset_reference_file()
+    {
+        $expectedFilePath = realpath(__DIR__ . '/../../fixtures/ziggy.png');
+
+        $this->server->setResponseOfPath(
+            '/'. sprintf(AssetReferenceFileApi::ASSET_REFERENCE_FILE_DOWNLOAD_URI, 'ziggy', 'en_US'),
+            new ResponseStack(
+                new Response(file_get_contents($expectedFilePath), [], 201)
+            )
+        );
+
+        $api = $this->createClient()->getAssetReferenceFileApi();
+        $downloadResponse = $api->downloadFromLocalizableAsset('ziggy', 'en_US');
+
+        Assert::assertSame($this->server->getLastRequest()->jsonSerialize()[RequestInfo::JSON_KEY_METHOD], 'GET');
+
+        Assert::assertInstanceOf(StreamInterface::class, $downloadResponse);
+        Assert::assertSame(file_get_contents($expectedFilePath), $downloadResponse->getContents());
+    }
+
+    public function test_download_a_not_localizable_asset_reference_file()
+    {
+        $expectedFilePath = realpath(__DIR__ . '/../../fixtures/ziggy-certification.jpg');
+
+        $this->server->setResponseOfPath(
+            '/'. sprintf(AssetReferenceFileApi::ASSET_REFERENCE_FILE_DOWNLOAD_URI, 'ziggy_certif', AssetReferenceFileApi::NOT_LOCALIZABLE_ASSET_LOCALE_CODE),
+            new ResponseStack(
+                new Response(file_get_contents($expectedFilePath), [], 201)
+            )
+        );
+
+        $api = $this->createClient()->getAssetReferenceFileApi();
+        $downloadResponse = $api->downloadFromNotLocalizableAsset('ziggy_certif');
+
+        Assert::assertSame($this->server->getLastRequest()->jsonSerialize()[RequestInfo::JSON_KEY_METHOD], 'GET');
+
+        $this->assertInstanceOf(StreamInterface::class, $downloadResponse);
+        $this->assertSame(file_get_contents($expectedFilePath), $downloadResponse->getContents());
+    }
+
+    /**
+     * @expectedException \Akeneo\Pim\ApiClient\Exception\NotFoundHttpException
+     */
+    public function test_download_from_localizable_asset_not_found()
+    {
+        $this->server->setResponseOfPath(
+            '/'. sprintf(AssetReferenceFileApi::ASSET_REFERENCE_FILE_DOWNLOAD_URI, 'ziggy', 'en_US'),
+            new ResponseStack(
+                new Response('{"code": 404, "message":"Not found"}', [], 404)
+            )
+        );
+
+        $api = $this->createClient()->getAssetReferenceFileApi();
+        $api->downloadFromLocalizableAsset('ziggy', 'en_US');
+    }
+
+    /**
+     * @expectedException \Akeneo\Pim\ApiClient\Exception\NotFoundHttpException
+     */
+    public function test_download_from_not_localizable_asset_not_found()
+    {
+        $this->server->setResponseOfPath(
+            '/'. sprintf(AssetReferenceFileApi::ASSET_REFERENCE_FILE_DOWNLOAD_URI, 'ziggy_certif', AssetReferenceFileApi::NOT_LOCALIZABLE_ASSET_LOCALE_CODE),
+            new ResponseStack(
+                new Response('{"code": 404, "message":"Not found"}', [], 404)
+            )
+        );
+
+        $api = $this->createClient()->getAssetReferenceFileApi();
+
+        $api->downloadFromNotLocalizableAsset('ziggy_certif');
+    }
+}
---
+++ tests/Api/AssetVariationFile/DownloadAssetVariationFileApiIntegration.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Akeneo\PimEnterprise\ApiClient\tests\v2_1\Api\AssetVariationFile;
+
+use Akeneo\PimEnterprise\ApiClient\Api\AssetVariationFileApi;
+use Akeneo\PimEnterprise\ApiClient\tests\Api\ApiTestCaseEnterprise;
+use donatj\MockWebServer\RequestInfo;
+use donatj\MockWebServer\Response;
+use donatj\MockWebServer\ResponseStack;
+use PHPUnit\Framework\Assert;
+use Psr\Http\Message\StreamInterface;
+
+class DownloadAssetVariationFileApiIntegration extends ApiTestCaseEnterprise
+{
+
+    public function test_download_a_localizable_asset_variation_file()
+    {
+        $expectedFilePath = realpath(__DIR__ . '/../../fixtures/ziggy.png');
+
+        $this->server->setResponseOfPath(
+            '/'. sprintf(AssetVariationFileApi::ASSET_VARIATION_FILE_DOWNLOAD_URI, 'ziggy', 'ecommerce', 'en_US'),
+            new ResponseStack(
+                new Response(file_get_contents($expectedFilePath), [], 201)
+            )
+        );
+
+        $api = $this->createClient()->getAssetVariationFileApi();
+        $downloadResponse = $api->downloadFromLocalizableAsset('ziggy', 'ecommerce', 'en_US');
+
+        Assert::assertSame($this->server->getLastRequest()->jsonSerialize()[RequestInfo::JSON_KEY_METHOD], 'GET');
+
+        Assert::assertInstanceOf(StreamInterface::class, $downloadResponse);
+        Assert::assertSame(file_get_contents($expectedFilePath), $downloadResponse->getContents());
+    }
+
+
+    public function test_download_a_not_localizable_asset_variation_file()
+    {
+        $expectedFilePath = realpath(__DIR__ . '/../../fixtures/ziggy-certification.jpg');
+
+        $this->server->setResponseOfPath(
+            '/'. sprintf(AssetVariationFileApi::ASSET_VARIATION_FILE_DOWNLOAD_URI, 'ziggy_certif', 'ecommerce', AssetVariationFileApi::NOT_LOCALIZABLE_ASSET_LOCALE_CODE),
+            new ResponseStack(
+                new Response(file_get_contents($expectedFilePath), [], 201)
+            )
+        );
+
+        $api = $this->createClient()->getAssetVariationFileApi();
+        $downloadResponse = $api->downloadFromNotLocalizableAsset('ziggy_certif', 'ecommerce');
+
+        Assert::assertSame($this->server->getLastRequest()->jsonSerialize()[RequestInfo::JSON_KEY_METHOD], 'GET');
+
+        Assert::assertInstanceOf(StreamInterface::class, $downloadResponse);
+        Assert::assertSame(file_get_contents($expectedFilePath), $downloadResponse->getContents());
+    }
+
+    /**
+     * @expectedException \Akeneo\Pim\ApiClient\Exception\NotFoundHttpException
+     */
+    public function test_download_from_localizable_asset_not_found()
+    {
+        $this->server->setResponseOfPath(
+            '/'. sprintf(AssetVariationFileApi::ASSET_VARIATION_FILE_DOWNLOAD_URI, 'ziggy', 'mobile', 'en_US'),
+            new ResponseStack(
+                new Response('{"code": 404, "message":"Not found"}', [], 404)
+            )
+        );
+
+        $api = $this->createClient()->getAssetVariationFileApi();
+        $api->downloadFromLocalizableAsset('ziggy', 'mobile', 'en_US');
+    }
+
+    /**
+     * @expectedException \Akeneo\Pim\ApiClient\Exception\NotFoundHttpException
+     */
+    public function test_download_from_not_localizable_asset_not_found()
+    {
+        $this->server->setResponseOfPath(
+            '/'. sprintf(AssetVariationFileApi::ASSET_VARIATION_FILE_DOWNLOAD_URI, 'ziggy_certif', 'mobile', AssetVariationFileApi::NOT_LOCALIZABLE_ASSET_LOCALE_CODE),
+            new ResponseStack(
+                new Response('{"code": 404, "message":"Not found"}', [], 404)
+            )
+        );
+
+        $api = $this->createClient()->getAssetVariationFileApi();
+        $api->downloadFromNotLocalizableAsset('ziggy_certif', 'mobile');
+    }
+}
