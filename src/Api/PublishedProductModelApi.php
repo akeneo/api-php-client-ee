8b1b0ca83d0e8eedbfc3a6ea2d93b1925a6c04e8
Elodie Raposo <raposo.elodie@gmail.com>
2018-06-19 16:11:44 +0200

    API : add Product Model published

---
+++ src/Api/PublishedProductModelApi.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Akeneo\PimEnterprise\ApiClient\Api;
+
+use Akeneo\Pim\ApiClient\Client\ResourceClientInterface;
+use Akeneo\Pim\ApiClient\Pagination\PageFactoryInterface;
+use Akeneo\Pim\ApiClient\Pagination\ResourceCursorFactoryInterface;
+
+/**
+ * API implementation to manage published product models.
+ *
+ * @author    Elodie Raposo <elodie.raposo@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class PublishedProductModelApi implements PublishedProductModelApiInterface
+{
+    const PUBLISHED_PRODUCT_MODELS_URI = 'api/rest/v1/published-product-models';
+    const PUBLISHED_PRODUCT_MODEL_URI = 'api/rest/v1/published-product-models/%s';
+
+    /** @var ResourceClientInterface */
+    protected $resourceClient;
+
+    /** @var PageFactoryInterface */
+    protected $pageFactory;
+
+    /** @var ResourceCursorFactoryInterface */
+    protected $cursorFactory;
+
+    /**
+     * @param ResourceClientInterface        $resourceClient
+     * @param PageFactoryInterface           $pageFactory
+     * @param ResourceCursorFactoryInterface $cursorFactory
+     */
+    public function __construct(
+        ResourceClientInterface $resourceClient,
+        PageFactoryInterface $pageFactory,
+        ResourceCursorFactoryInterface $cursorFactory
+    ) {
+        $this->resourceClient = $resourceClient;
+        $this->pageFactory = $pageFactory;
+        $this->cursorFactory = $cursorFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get($code)
+    {
+        return $this->resourceClient->getResource(static::PUBLISHED_PRODUCT_MODEL_URI, [$code]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function listPerPage($limit = 10, $withCount = false, array $queryParameters = [])
+    {
+        $data = $this->resourceClient->getResources(
+            static::PUBLISHED_PRODUCT_MODELS_URI,
+            [],
+            $limit,
+            $withCount,
+            $queryParameters
+        );
+
+        return $this->pageFactory->createPage($data);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function all($pageSize = 10, array $queryParameters = [])
+    {
+        $queryParameters['pagination_type'] = 'search_after';
+
+        $firstPage = $this->listPerPage($pageSize, false, $queryParameters);
+
+        return $this->cursorFactory->createCursor($pageSize, $firstPage);
+    }
+}
